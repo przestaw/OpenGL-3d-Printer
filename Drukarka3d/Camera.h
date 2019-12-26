606e81d1b569c5aedfc65caa67518b9a5ac18c09
Temebe <seboado@gmail.com>
2019-12-26 11:59:56 +0100

    chore: init camera class

---
+++ Drukarka3d/Camera.h
@@ -0,0 +1,51 @@
+#pragma once
+
+#include <glew.h>
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+
+#include <vector>
+
+enum Direction {
+	FORWARD,
+	BACKWARD,
+	LEFT,
+	RIGHT
+};
+
+// Default camera values
+const GLfloat YAW = -90.0f;
+const GLfloat PITCH = 0.0f;
+const GLfloat SPEED = 2.5f;
+const GLfloat SENSITIVITY = 0.1f;
+const GLfloat ZOOM = 45.0f;
+
+
+class Camera
+{
+	Camera(glm::vec3 position = glm::vec3(0.0f, 0.0f, 0.0f), glm::vec3 up = glm::vec3(0.0f, 1.0f, 0.0f),
+		   GLfloat yaw = YAW, GLfloat pitch = PITCH);
+
+	// Returns view matrix for scene
+	glm::mat4 getView();
+
+	// Process events
+	void handleKeyboard(Direction direction, GLfloat deltaTime);
+	void handleMouseMovement(GLfloat xOffset, GLfloat yOffset);
+	void handleMouseScroll(GLfloat yOffset);
+
+	// Sets boundaries for camera meaning that camera will not leave a cuboid made out of two opposite vertices
+	void setBoundries(glm::vec3 leftFrontBottom, glm::vec3 rightBackTop);
+	void unsetBoundries();
+
+	// Sets constrains for how far up can camera look
+	void setPitchConstrains(GLfloat minPitch, GLfloat maxPitch);
+	void unsetPitchConstraint();
+
+	// Sets camera position in an instant
+	void moveCamera(glm::vec3 position);
+
+	GLfloat getZoom();
+
+};
+
