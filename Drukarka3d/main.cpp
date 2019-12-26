9aed92a8f1a8b269627573a996c1f66fd5b92880
Temebe <seboado@gmail.com>
2019-12-26 16:01:29 +0100

    fix: bring back non resizable window

--- Drukarka3d/main.cpp
+++ Drukarka3d/main.cpp
@@ -69,7 +69,7 @@ int main() {
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 	glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
 
-	//glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
+	glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
 
 	try
 	{
